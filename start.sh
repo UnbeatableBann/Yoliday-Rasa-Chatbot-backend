0a29d8653d6ef94170dbcee749109443df26fa2b
Shadab Jamadar <140196120+UnbeatableBann@users.noreply.github.com>
2025-06-04 13:27:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 echo "Starting Rasa and Action Server..."
 echo "PORT is set to: $PORT"
 rasa run actions --port 5055 &
-rasa run --enable-api --cors "*" --port $PORT
+rasa run --enable-api --cors "*" --port $PORT --model models/20250604-122941-stale-bus.tar.gz
