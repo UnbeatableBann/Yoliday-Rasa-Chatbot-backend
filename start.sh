c7aa7ad4570b05a2bea1a4a6785fc2796b2d7a06
Shadab Jamadar <140196120+UnbeatableBann@users.noreply.github.com>
2025-06-04 11:44:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "Starting Rasa and Action Server..."
-ls -l
-rasa run actions --port 5055 --debug &
-rasa run --enable-api --cors "*" --port $PORT --debug
+rasa run actions --port 5055 &
+rasa run --enable-api --cors "*" --port $PORT
