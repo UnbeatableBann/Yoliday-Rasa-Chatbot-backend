a98f1611512a9bc570d85256da100fe1260e7f5e
Shadab Jamadar <140196120+UnbeatableBann@users.noreply.github.com>
2025-06-04 12:40:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 echo "Starting Rasa and Action Server..."
 rasa run actions --port 5055 &
-rasa run --enable-api --cors "*" --port 5005 &
-echo "Starting frontend on port $PORT..."
-python3 -m http.server $PORT --directory ./frontend
+rasa run --enable-api --cors "*" --port $PORT
